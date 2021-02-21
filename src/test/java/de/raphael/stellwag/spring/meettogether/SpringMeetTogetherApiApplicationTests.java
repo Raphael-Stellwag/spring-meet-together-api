0b4d2ef98e9986a1954e81be5df1014d50df39ed
Raphael Stellwag <raphael.stellwag@gmx.de>
2021-02-21 22:04:35 +0100

    fix test cases

--- src/test/java/de/raphael/stellwag/spring/meettogether/SpringMeetTogetherApiApplicationTests.java
+++ src/test/java/de/raphael/stellwag/spring/meettogether/SpringMeetTogetherApiApplicationTests.java
@@ -1,13 +1,19 @@
 package de.raphael.stellwag.spring.meettogether;
 
+import org.junit.Assert;
 import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
 
-@SpringBootTest
+@RunWith(SpringRunner.class)
+@SpringBootTest(classes = SpringMeetTogetherApiApplication.class,
+		webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 class SpringMeetTogetherApiApplicationTests {
 
 	@Test
 	void contextLoads() {
+		Assert.assertTrue(true);
 	}
 
 }
